ea631f756c6d8693bfa98318427caa61e5cf9d52
harisishtiaq1 <harisharry232@gmail.com>
2023-05-16 12:52:03 +0500

    updated

--- src/components/AppWrapper.js
+++ src/components/AppWrapper.js
@@ -1,9 +1,19 @@
-import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  List,
+  ListItemText,
+  Popover,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { auth } from "../firebase-config.js";
 import { signOut } from "firebase/auth";
-
+import PersonIcon from "@mui/icons-material/Person";
 import Cookies from "universal-cookie";
-
+import React from "react";
 const cookies = new Cookies();
 
 export const AppWrapper = ({ children, isAuth, setIsAuth, setIsInChat }) => {
@@ -13,6 +23,18 @@ export const AppWrapper = ({ children, isAuth, setIsAuth, setIsInChat }) => {
     setIsAuth(false);
     setIsInChat(false);
   };
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
 
   return (
     <>
@@ -23,33 +45,53 @@ export const AppWrapper = ({ children, isAuth, setIsAuth, setIsInChat }) => {
             alignItems: "center",
             justifyContent: "space-between",
             fontWeight: "500",
-            // borderRadius: "0px 40px 0px 40px",
             backgroundColor: "blueViolet",
             fontStyle: "italic",
             color: "white",
           }}
         >
-          <Typography>Chat App</Typography>
+          <Typography sx={{ fontSize: "20px" }}>Chat App</Typography>
           {isAuth && (
             <Box
               sx={{
                 display: "flex",
                 justifyContent: "center",
                 alignItems: "center",
-                // mt: 3,
                 position: "absolute",
                 right: 20,
               }}
             >
-              <Button variant="contained" onClick={signUserOut}>
-                Sign Out
-              </Button>
+              <IconButton onClick={handleClick} sx={{ color: "white", mr: 5 }}>
+                <PersonIcon fontSize="large" />
+              </IconButton>
             </Box>
           )}
         </Toolbar>
       </AppBar>
-
       <Box sx={{ mt: 8 }}>{children}</Box>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+      >
+        <List sx={{ width: "100px", ml: 1 }}>
+          <ListItemText sx={{ cursor: "pointer" }}>
+            <Typography>User Profile</Typography>
+          </ListItemText>
+          <ListItemText onClick={signUserOut} sx={{ cursor: "pointer" }}>
+            <Typography>Sign Out</Typography>
+          </ListItemText>
+        </List>
+      </Popover>
     </>
   );
 };
--- src/components/Chat.js
+++ src/components/Chat.js
@@ -11,16 +11,31 @@ import {
 } from "firebase/firestore";
 import {
   Box,
-  Button,
-  Input,
+  IconButton,
+  Paper,
   Stack,
   TextField,
   Typography,
 } from "@mui/material";
-
+import { Recorder } from "react-voice-recorder";
+import "react-voice-recorder/dist/index.css";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import SendIcon from "@mui/icons-material/Send";
+import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
+import CameraAltIcon from "@mui/icons-material/CameraAlt";
+import KeyboardVoiceIcon from "@mui/icons-material/KeyboardVoice";
+import Scrollbars from "react-custom-scrollbars";
+const thumbStyle = {
+  backgroundColor: "transparent",
+  borderRadius: "6px",
+  cursor: "pointer",
+  width: "3px",
+  height: "5px",
+};
 export const Chat = ({ room }) => {
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState("");
+  const [timeStamp, setTimeStamp] = useState(null);
   const messagesRef = collection(db, "messages");
 
   useEffect(() => {
@@ -47,19 +62,18 @@ export const Chat = ({ room }) => {
 
     return () => unsubscribe();
   }, []);
-  console.log("messages", messages);
-
   const handleSubmit = async (event) => {
     event.preventDefault();
-    // const  uid  = auth.currentUser;
     setNewMessage("");
+    const timeStamp = serverTimestamp();
     if (newMessage === "") return;
     await addDoc(messagesRef, {
       text: newMessage,
-      createdAt: serverTimestamp(),
+      createdAt: timeStamp,
       user: auth.currentUser.displayName,
       room,
     });
+    setTimeStamp(timeStamp);
   };
 
   return (
@@ -70,68 +84,22 @@ export const Chat = ({ room }) => {
         mt: 2,
       }}
     >
-      {messages &&
-        messages.map((message) => (
-          <>
-            {message.user === auth.currentUser?.displayName ? (
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "flex-end",
-                }}
-              >
-                <Box
-                  key={message.id}
-                  sx={{
-                    borderRadius: "10px 10px 10px 10px",
-                    display: "flex",
-                    flexDirection: "row",
-                    margin: "5px",
-                    backgroundColor: "blue",
-                    width: "fit-content",
-                    justifyContent: "flex-end",
-                    color: "white",
-                  }}
-                >
-                  <Typography
-                    variant="h5"
-                    component="h5"
-                    sx={{
-                      fontSize: "16px",
-                      fontWeight: "500",
-                      ml: 2,
-                      mt: 2,
-                      fontStyle: "italic",
-                    }}
-                  >
-                    {message.user}:
-                  </Typography>
-                  <Typography
-                    variant="h6"
-                    component="h6"
-                    sx={{
-                      ml: 1,
-                      mr: 1,
-                      mt: 2,
-                      fontSize: "12px",
-                      textTransform: "capitalize",
-                      height: "50px",
-                      fontWeight: "400",
-                    }}
-                  >
-                    {message.text}
-                  </Typography>
-                </Box>
-              </Box>
-            ) : (
-              (console.log("info here", message.user, auth.currentUser),
-              (
+      <Scrollbars
+        style={{ height: 490 }}
+        autoHide={true}
+        renderThumbVertical={({ style, ...props }) => (
+          <div {...props} style={{ ...style, ...thumbStyle }} />
+        )}
+      >
+        {messages &&
+          messages.map((message) => (
+            <>
+              {message.user === auth.currentUser?.displayName ? (
                 <Box
                   sx={{
                     display: "flex",
                     flexDirection: "column",
-                    // alignItems: "flex-end",
+                    alignItems: "flex-end",
                   }}
                 >
                   <Box
@@ -140,36 +108,23 @@ export const Chat = ({ room }) => {
                       borderRadius: "10px 10px 10px 10px",
                       display: "flex",
                       flexDirection: "row",
-                      margin: "10px",
-                      backgroundColor: "grey",
-                      color: "black",
+                      margin: "5px",
+                      backgroundColor: "blue",
                       width: "fit-content",
                       justifyContent: "flex-end",
+                      color: "white",
                     }}
                   >
-                    <Typography
-                      variant="h5"
-                      component="h5"
-                      sx={{
-                        fontWeight: "500",
-                        ml: 2,
-                        mt: 2,
-                        fontStyle: "italic",
-                        fontSize: "16px",
-                      }}
-                    >
-                      {message.user}:
-                    </Typography>
                     <Typography
                       variant="h6"
                       component="h6"
                       sx={{
-                        fontSize: "14px",
                         ml: 1,
                         mr: 1,
                         mt: 2,
+                        fontSize: "16px",
                         textTransform: "capitalize",
-                        height: "50px",
+                        height: "40px",
                         fontWeight: "400",
                       }}
                     >
@@ -177,32 +132,118 @@ export const Chat = ({ room }) => {
                     </Typography>
                   </Box>
                 </Box>
-              ))
-            )}
-          </>
-        ))}
+              ) : (
+                (console.log("info here", message.user, auth.currentUser),
+                (
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "column",
+                    }}
+                  >
+                    <Box
+                      key={message.id}
+                      sx={{
+                        borderRadius: "10px 10px 10px 10px",
+                        display: "flex",
+                        flexDirection: "row",
+                        margin: "10px",
+                        backgroundColor: "grey",
+                        color: "black",
+                        width: "fit-content",
+                        justifyContent: "flex-end",
+                      }}
+                    >
+                      <Typography
+                        variant="h6"
+                        component="h6"
+                        sx={{
+                          fontSize: "16px",
+                          ml: 1,
+                          mr: 1,
+                          mt: 2,
+                          textTransform: "capitalize",
+                          height: "40px",
+                          fontWeight: "400",
+                        }}
+                      >
+                        {message.text}
+                      </Typography>
+                      {timeStamp && (
+                        <Typography>
+                          {timeStamp.toDate().toLocaleString()}
+                        </Typography>
+                      )}
+                    </Box>
+                  </Box>
+                ))
+              )}
+            </>
+          ))}
+      </Scrollbars>
 
-      <form onSubmit={handleSubmit} className="new-message-form">
-        <Stack
-          direction="row"
-          sx={{ mb: 2, mt: 2, position: "fixed", bottom: 0, width: "100%" }}
-        >
-          <TextField
-            type="text"
-            fullWidth
-            placeholder="Type your message here"
-            value={newMessage}
-            onChange={(event) => setNewMessage(event.target.value)}
-            className="new-message-input"
-          />
-          <Button
-            sx={{ ml: 3, padding: 2, width: 150 }}
-            variant="contained"
-            type="submit"
-            className="send-button"
+      <form onSubmit={handleSubmit}>
+        <Stack direction="row" sx={{ mb: 2, mt: 2, bottom: 0, width: "100%" }}>
+          <Paper
+            sx={{
+              width: "100%",
+              display: "flex",
+            }}
           >
-            Send
-          </Button>
+            <IconButton>
+              <EmojiEmotionsIcon />
+            </IconButton>
+            <TextField
+              sx={{ border: "none", borderRadius: 0 }}
+              type="text"
+              fullWidth
+              placeholder="Type Something"
+              value={newMessage}
+              autoComplete="off"
+              onChange={(event) => setNewMessage(event.target.value)}
+            />
+            <IconButton>
+              <CameraAltIcon />
+            </IconButton>
+            <IconButton>
+              <AttachFileIcon />
+            </IconButton>
+          </Paper>
+          {newMessage.length > 0 ? (
+            <IconButton
+              sx={{
+                ml: 3,
+                padding: 2,
+                width: 150,
+                backgroundColor: "lightblue",
+                borderRadius: "10px",
+                "&:hover": {
+                  backgroundColor: "lightblue",
+                },
+              }}
+              variant="contained"
+              type="submit"
+            >
+              <SendIcon />
+            </IconButton>
+          ) : (
+            <IconButton
+              sx={{
+                ml: 3,
+                padding: 2,
+                width: 150,
+                backgroundColor: "lightblue",
+                borderRadius: "10px",
+                "&:hover": {
+                  backgroundColor: "lightblue",
+                },
+              }}
+              variant="contained"
+              type="submit"
+            >
+              <KeyboardVoiceIcon />
+            </IconButton>
+          )}
         </Stack>
       </form>
     </Box>
